5ecfb7961134da7e35ddefcbbdf6133ab2e3f96c
Professional <okijhhyu@gmail.com>
2025-01-03 04:56:07 +0300

    fix: fixed link

--- pages/modules/[module]/[id].vue
+++ pages/modules/[module]/[id].vue
@@ -1044,7 +1044,7 @@ const available = computed(() => {
 // Initialize stores and route
 const sectionStore = useSectionsStore();
 const modulesStore = useModulesStore();
-const { sendMessage, data } = useWebSocket('wss:https://task530.onrender.com:443/', carusel);
+const { sendMessage, data } = useWebSocket('wss://task530.onrender.com:443/', carusel);
 
 const send = () => {
   sendMessage("Обновить данные");
